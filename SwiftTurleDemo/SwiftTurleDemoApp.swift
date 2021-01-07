ddd132d855d022566cd0d7b6cfdfa5a50a8e3704
Taziamoma Abraham <taziamoma@gmail.com>
2021-01-07 15:30:00 -0600

    Initial Commit

---
+++ SwiftTurleDemo/SwiftTurleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTurleDemoApp.swift
+//  SwiftTurleDemo
+//
+//  Created by Taziamoma Abraham on 1/7/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTurleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
